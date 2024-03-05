f371e24f723708408eab4988d914756bddf95e5d
danielfolse <90349765+danielfolse@users.noreply.github.com>
2024-03-04 21:22:14 -0700

    adding

--- provider/app.py
+++ provider/app.py
@@ -3,8 +3,7 @@ import logging
 from connexion.exceptions import Unauthorized
 from flask import abort, request, current_app as app
 
-from __init__ import UpstreamProviderError
-import provider
+from . import UpstreamProviderError, provider
 
 logger = logging.getLogger(__name__)
 AUTHORIZATION_HEADER = "Authorization"
--- provider/provider.py
+++ provider/provider.py
@@ -8,8 +8,7 @@ from google.oauth2.credentials import Credentials
 from googleapiclient.discovery import build
 from googleapiclient.errors import HttpError
 
-from __init__ import UpstreamProviderError
-import async_download
+from . import UpstreamProviderError, async_download
 
 CSV_MIMETYPE = "text/csv"
 TEXT_MIMETYPE = "text/plain"
